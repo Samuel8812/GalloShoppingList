aee8bbf9de6a3839ce9ba0186325e736d91868e3
Samuel8812 <samuelbardelicanal@gmail.com>
2026-05-20 15:07:40 -0300

    aula 6

--- GalloShoppingList/app/_layout.jsx
+++ GalloShoppingList/app/_layout.jsx
@@ -1,16 +1,15 @@
-import { Stack } from 'expo-router'
-import { StatusBar } from 'react-native' 
-import React from 'react'
+import React from 'react';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
     <>
       <Stack>
-        <Stack.Screen
-        name="index" options={{ headerShown: false}}/>
-        <Stack.Screen
-        name="home" options={{headerShown: false}}/>
+        <Stack.Screen name='index' options={{ headerShown : false}} />
+        <Stack.Screen name='home' options={{ headerShown : false}} />
       </Stack>
+      <StatusBar style='light' />
     </>
   )
 }
